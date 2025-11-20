bdad59edc83bc8cac264e3e7199fdda775385c27
ChverkoGleb <chverkogleb@bk.ru>
2025-11-20 22:51:52 +0700

    Реагирование на клики

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -13,6 +13,7 @@ import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
 import androidx.glance.LocalSize
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.cornerRadius
@@ -93,6 +94,11 @@ class StockAppWidget : GlanceAppWidget() {
             }
         }
     }
+
+    private fun refreshPrice() {
+        PriceDataRepo.update()
+    }
+
     @Composable
     fun GlanceContent() {
         val stateCount by PriceDataRepo.currentPrice.collectAsState()
@@ -105,6 +111,7 @@ class StockAppWidget : GlanceAppWidget() {
     @Composable
     private fun Small(stateCount: Float) {
         Column(modifier = GlanceModifier
+            .clickable { refreshPrice() }
             .fillMaxSize()
             .background(GlanceTheme.colors.background)
             .padding(8.dp)) {
@@ -115,6 +122,7 @@ class StockAppWidget : GlanceAppWidget() {
     private fun Medium(stateCount: Float) {
         Column(horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
             modifier = GlanceModifier
+                .clickable { refreshPrice() }
                 .fillMaxSize()
                 .cornerRadius(15.dp)
                 .background(GlanceTheme.colors.background)
